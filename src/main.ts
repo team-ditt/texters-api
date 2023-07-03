0a0f58c2131505e0c02868e922a604f4160525bb
zake-dev <floatim00@gmail.com>
2023-07-03 17:57:53 +1000

    dev: add global api prefix

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const PORT = configService.get<string>("PORT");
+
+  app.setGlobalPrefix("api/v1");
   app.useGlobalPipes(new ValidationPipe({transform: true}));
   await app.listen(parseInt(PORT) || 3000);
 }
