5393de2dc0d225d2419f1de6c06750903bee5342
zake-dev <floatim00@gmail.com>
2023-07-05 10:25:16 +1000

    refactor: unused import

--- src/features/auth/auth.module.ts
+++ src/features/auth/auth.module.ts
@@ -4,7 +4,6 @@ import {Module} from "@nestjs/common";
 import {ConfigModule, ConfigService} from "@nestjs/config";
 import {JwtModule} from "@nestjs/jwt";
 import {TypeOrmModule} from "@nestjs/typeorm";
-import {AuthGuard} from "../common/auth.guard";
 import {AuthController} from "./auth.controller";
 import {AuthService} from "./auth.service";
 import {Auth} from "./model/auth.entity";
@@ -24,7 +23,7 @@ import {Auth} from "./model/auth.entity";
     TypeOrmModule.forFeature([Auth]),
     MembersModule,
   ],
-  providers: [AuthGuard, AuthService],
+  providers: [AuthService],
   controllers: [AuthController],
 })
 export class AuthModule {}
