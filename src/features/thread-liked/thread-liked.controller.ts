3e10bac8988832678a3e3ede81ba26e2207b15c6
zake-dev <floatim00@gmail.com>
2023-08-28 19:03:08 +1000

    hotfix: wrong ip address collected on thread liked

--- src/features/thread-liked/thread-liked.controller.ts
+++ src/features/thread-liked/thread-liked.controller.ts
@@ -1,6 +1,6 @@
 import {OptionalAuthGuard} from "@/features/auth/optional-auth.guard";
 import {ThreadLikedService} from "@/features/thread-liked/thread-liked.service";
-import {Controller, HttpCode, HttpStatus, Ip, Param, Post, Req, UseGuards} from "@nestjs/common";
+import {Controller, HttpCode, HttpStatus, Param, Post, Req, UseGuards} from "@nestjs/common";
 import {Request} from "express";
 
 @Controller()
@@ -10,11 +10,7 @@ export class ThreadLikedController {
   @Post("/boards/:boardId/threads/:threadId/liked")
   @UseGuards(OptionalAuthGuard)
   @HttpCode(HttpStatus.NO_CONTENT)
-  async increaseThreadLiked(
-    @Req() req: Request,
-    @Ip() ip: string,
-    @Param("threadId") threadId: number,
-  ) {
-    await this.threadLikedService.increaseThreadLiked(threadId, ip, req["member"]);
+  async increaseThreadLiked(@Req() req: Request, @Param("threadId") threadId: number) {
+    await this.threadLikedService.increaseThreadLiked(threadId, req["clientIp"], req["member"]);
   }
 }
